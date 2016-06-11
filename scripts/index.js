c4e92fa26a66b83c11823a38c49621bbba69296e
MrTimcakes <MrTimcakes@gmail.com>
2016-06-11 22:02:31 +0100

    Script to rebuild Electron

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,3 @@
-var FFI = require('ffi');
+var hide = require('node-hide')
 
-function TEXT(text){
-  return new Buffer(text, 'ucs2').toString('binary');
-}
-
-var user32 = new FFI.Library('user32', {
-  'FindWindowW': ['int', ['string', 'string']],
-  'ShowWindow': ['int', ['int', 'int']]
-});
-
-var handle = user32.FindWindowW(null, TEXT('Untitled - Notepad'));
-console.log(handle);
-user32.ShowWindow(handle, 0);
-setTimeout(function(){
-  user32.ShowWindow(handle, 1);
-}, 5000);
+hide.visableWindows(function(e){console.log(JSON.stringify(e))})
