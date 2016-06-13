fd9c377b1d5ebdfb9105fd0d7ae38fa1bb9d2c2f
MrTimcakes <MrTimcakes@gmail.com>
2016-06-13 23:21:53 +0100

    Implemented Window list fill

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
 var hide = require('node-hide')
+var openWindows;
 
-hide.visableWindows(function(e){console.log(JSON.stringify(e))})
+hide.visableWindows(function(e){openWindows = e;})
+
+function fillWindowList(){
+  hide.visableWindows(function(data){
+    var list = document.getElementById('windowList');
+    while(list.hasChildNodes()) {
+      list.removeChild(list.lastChild);
+    }
+    list
+    for (var handle in data) {
+      var li = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.id = handle;
+
+      var label = document.createElement('label')
+      label.htmlFor = handle;
+      label.appendChild(document.createTextNode(data[handle]));
+
+      li.appendChild(checkbox);
+      li.appendChild(label);
+      list.appendChild(li);
+    }
+  })
+}
+
+fillWindowList();
